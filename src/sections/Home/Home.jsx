f1cee15533049dfef5d039f2a09bac47c025171b
Nicolas Choquet <nicolachoquet06250@gmail.com>
2022-11-21 00:44:48 +0100

    meilleur gestion du responsive pour le logo de la home page

--- src/sections/Home/Home.jsx
+++ src/sections/Home/Home.jsx
@@ -3,8 +3,11 @@ import DownButton from "../../components/DownButton/DownButton";
 import OverlayedSection from '../../layouts/OverlayedSection/OverlayedSection';
 import imageHome from '../../assets/image-home.jpg';
 import logo from '../../assets/logo-hakim-benomara-v6-dark.svg';
+import { useEffect, useState } from 'react';
 
 export default function Home() {
+    const [styleLogoClass, setStyleLogoClass] = useState('horizontal');
+
     const onClick = () => {
         const goTo = 'second-section';
         const targetSection = document.querySelector(`section.${goTo}`);
@@ -17,11 +20,28 @@ export default function Home() {
         }
     };
 
+    useEffect(() => {
+        const handleResize = () => {
+            if (window.innerWidth > window.innerHeight) {
+                setStyleLogoClass('horizontal');
+            } else if (window.innerWidth < window.innerHeight) {
+                setStyleLogoClass('vertical');
+            }
+        };
+        handleResize();
+
+        window.addEventListener('resize', handleResize);
+
+        return () => {
+            window.removeEventListener('resize', handleResize);
+        }
+    }, [])
+
     return (<OverlayedSection>
         <img src={imageHome} alt="logo home" className={styles.backImg} />
 
         <div className={styles.completeLogoContainer}>
-            <img src={logo} alt="logo complet" />
+            <img src={logo} alt="logo complet" className={styles[styleLogoClass + 'Logo']} />
         </div>
 
         <div className={styles.downButtonContainer}>
